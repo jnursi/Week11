e0f396cfae2005194475ad417acd042a5bdd33b8
jnursi <jnursi>
2021-12-22 14:17:33 +0200

    fix images

--- js/randomImage.js
+++ js/randomImage.js
@@ -1,6 +1,6 @@
 const myButton = document.querySelector('button');
 let image = document.querySelector ('img');
-const images = ["/images/pic1.jpeg", "/images/pic2.jpeg", "/images/pic3.jpeg", "/images/pic4.jpeg"];
+const images = ["images/pic1.jpeg", "images/pic2.jpeg", "images/pic3.jpeg", "images/pic4.jpeg"];
 
 
 myButton.addEventListener('click', changeImage);
